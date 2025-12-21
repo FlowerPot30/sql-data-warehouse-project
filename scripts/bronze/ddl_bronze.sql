b523b806cf7e184e21968d32ef7cdbc8f6db6313
Thanaphut Aksornprasert <117530788+FlowerPot30@users.noreply.github.com>
2025-12-21 15:18:53 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -88,8 +88,8 @@ GO
 CREATE TABLE bronze.erp_products (
 	product_id VARCHAR(50),
 	product_category_name VARCHAR(50),
-	product_name_lenght INT,
-	product_description_lenght INT,
+	product_name_length INT,
+	product_description_length INT,
 	product_photos_qty INT,
 	product_weight_g INT,
 	product_length_cm INT,
